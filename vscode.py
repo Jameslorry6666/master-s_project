35630b147b23b49613bff962144e899338ca1e18
James <oluwasegunjamesilori@gmail.com>
2025-10-21 16:06:44 +0700

    updating my machinelearning model

--- vscode.py
+++ vscode.py
@@ -20,20 +20,75 @@ with st.expander("Data"):
  df
 
 
-#data cleaning 
-with st.expander("Data"):
+#data cleaning with st.expander("Data"):
+with st.expander("Data_Preprocessing"):
   st.write("**Data_Cleaning**")
   st.write(df.isnull().sum().sum())
 
 #data shape
-with st.expander("Data"):
- st.write("**Data shape**")
- st.write(df.shape)
+
+  st.write("**Data shape**")
+  st.write(df.shape)
 
 
-with st.expander("Data"):
- st.write("**Data describe**")
- st.write(df.describe())
+
+  st.write("**Data describe**")
+  st.write(df.describe())
+
+  # preparing data for machine learning training
+
+  st.write("**Data**")
+  st.write("**X**")
+  X =df
+  X
+
+
+  
+
+  st.write(X['Hand'].value_counts())
+  st.write(X['M/F'].value_counts())
+  st.write(X['Group'].value_counts())
+
+
+  #mapping 
+  st.dataframe(X['M/F'])
+
+  dictionary={'M':0, 'F':1}
+  X['GENDER']= X['M/F'].map(dictionary)
+  st.write( X['GENDER']) #'M':0, 'F':1
+
+  
+
+
+
+
+  dictionary = {'Demented':1, 'Nondemented':0,'Converted':1 }
+  X['Targets']= X['Group'].map(dictionary)
+  
+  st.write("**new_df**")
+  new_df = X[['Visit', 'MR Delay','Age','EDUC','SES','MMSE','eTIV','nWBV','ASF','CDR','GENDER','Targets']]
+  new_df 
+
+with st.sidebar:
+  st.header("Features_Input")
+  #visit= st.selectbox('Visit',1,2,3,4,5)
+"Visit', 'MR Delay','Age','EDUC','SES','MMSE','eTIV','nWBV','ASF','CDR','GENDER','Targets'"
+
+
+with st.expander("Features_Targets"):
+
+  X = new_df.drop(['MR Delay','Targets'], axis=1)
+  st.write("**X**")
+  X
+
+
+  st.write("**y**")
+  y = new_df['Targets']
+  y
+
+
+
+
 
 
 
